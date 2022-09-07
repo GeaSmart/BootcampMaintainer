f8e40836a89c35bfb572743b1966bda5f7630a73
Gerson Azabache <gerson.azabache@jalasoft.com>
2022-09-07 15:37:21 -0500

    initial commit

---
+++ BootcampMaintainer/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BootcampMaintainer.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
